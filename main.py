b95377062d33049e2de6ab11fff4c59091f5e6c6
Алексей Степыгин <snoy77@mail.ru>
2021-12-19 15:22:14 +0500

    start

---
+++ main.py
@@ -0,0 +1,20 @@
+import vk_api
+import WIA
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+vk_session = vk_api.VkApi(token=WIA.TOKEN)
+sesion_api = vk_session.get_api()
+long_pull = VkLongPoll(vk_session)
+
+
+def getUserName(id):
+	r = vk_session.method('users.get', {'user_ids': id})[0]
+	return str(str(r['id']) + " " + r['first_name'] + " " + r['last_name'])
+for event in long_pull.listen():
+	if event.type == VkEventType.MESSAGE_NEW:
+		if event.to_me:
+
+			user_id = event.user_id
+			message = event.text.lower()
+			print(getUserName(user_id) + ": \"" + event.text + "\"")
+
